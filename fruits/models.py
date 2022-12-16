42b6f1b774d035a66d2324ddc54ac2651a96dd5c
Daniel <Danielsharshenbekov04@gmail.com>
2022-12-16 18:08:14 +0600

    created model Type

--- fruits/models.py
+++ fruits/models.py
@@ -12,3 +12,14 @@ class Fruits(models.Model):
     class Meta:
         verbose_name = 'Фрукт'
         verbose_name_plural = 'Фрукты'
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Тип'
+        verbose_name_plural = 'Типы'
